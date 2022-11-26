f4ac92ed54ec6ddc7fb2ae2302c9a3ef84c7aade
Chollasit <vi.chollasit_st@tni.ac.th>
2022-11-26 11:26:29 +0700

    26/11/2022

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,19 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  // res.send('Hello, Kitty!');
+  res.status(200).json({
+    fullname: "Chollasit Vibulsirikul"
+  })
 });
+
+router.get('/bio', function(req, res, next){
+  res.status(200).json({
+    fullname: "Chollasit Vibulsirikul",
+    nickname: "Chin",
+    hobby: "Play Games",
+    gitusername: "chollasit-dev"
+  })
+})
 
 module.exports = router;
