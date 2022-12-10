bf775a9014bb3fa6523ad0b9cf47c104edf10ed4
Chollasit <vi.chollasit_st@tni.ac.th>
2022-12-10 14:03:48 +0700

    GetAllStaff

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const mongoose = require('mongoose');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var companyRouter = require('./routes/company');
+const staffController = require('./routes/staff');
 
 var app = express();
 mongoose.connect('mongodb+srv://superdev:y8DQvfeHXeXY3su2@2013210402-chollasit.ushw5qu.mongodb.net/restfulapi?retryWrites=true&w=majority', {useNewUrlParser: true, useUnifiedTopology: true});     //Password replace <password>
@@ -17,8 +18,11 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+const staffRouter = require('./routes/staff')
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/company', companyRouter);
+app.use('/staff', staffRouter);
 
 module.exports = app;
---
+++ controllers/staffController.js
@@ -0,0 +1,26 @@
+const Staff = require('../models/staff');
+
+exports.staff = async(req, res, next) => {
+
+  const staff = await Staff.find()
+
+    res.status(200).json({
+      data: staff
+    })
+}
+
+exports.insert = async(req, res, next) => {
+
+    const { name, salary } = req.body
+
+    let staff = new Staff({
+      name: name,
+      salary: salary
+    });
+    // let staff = new Staff(req.body);
+    await staff.save();
+
+    res.status(200).json({
+      message: ' เพิ่มข้อมูลเรียบร้อยแล้ว',
+    })
+}
