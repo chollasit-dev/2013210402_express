5b8d8f4f8bf4b2c6f42f3d254466b61273901a89
Chollasit <vi.chollasit_st@tni.ac.th>
2022-12-24 09:58:21 +0700

    Chapter5WorkshopA

--- controllers/shopController.js
+++ controllers/shopController.js
@@ -2,9 +2,9 @@ const Staff = require('../models/shop');
 
 exports.index = async(req, res, next) => {
 
-  const shop = await Staff.find().sort({_id: -1}) //1: Asc, -1: Desc
+  const shop = await Staff.find().select('name photo location').sort({_id:-1})
 
     res.status(200).json({
-      data: staff
+      data: shop
     })
 }
